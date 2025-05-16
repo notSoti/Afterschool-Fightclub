a544e2424bbbd123ee01a077f011ff38ec5d8e00
ieatpaintcookies <FoxSkinsForAll@gmail.com>
2025-05-16 19:33:51 +0300

    Main Menu polish and UI staff
    -Main Menu: Code polished up (moving some functions into script functions) and navigation smoothed for no need of mouse, can be used with keyboard or controller.
    -UI: Better-ed some buttons appearance in character selection. Improved in-game UI appearance with new ult charge circle, newly added player icons and "nests" for the healthbars and icons

--- My project/Assets/Scripts/MainMenu.cs
+++ My project/Assets/Scripts/MainMenu.cs
@@ -1,14 +1,53 @@
 
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
 
 public class MainMenu : MonoBehaviour {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+  
+    public GameObject mainMenu, optionsMenu, controlsPanel;
+
+    public GameObject optionsFirstButton, optionsClosedButton, controlsFirstButton, controlsClosedButton;
+
+
     public void PlayGame() {
         SceneManager.LoadScene("Character Selection");
     }
 
+    public void OpenOptions()
+    {
+        optionsMenu.SetActive(true);
+        mainMenu.SetActive(false);
+        EventSystem.current.SetSelectedGameObject(null); //unselect previous object
+        EventSystem.current.SetSelectedGameObject(optionsFirstButton);
+    }
+
+    public void CloseOptions()
+    {
+        optionsMenu.SetActive(false);
+        mainMenu.SetActive(true);
+        EventSystem.current.SetSelectedGameObject(null); //unselect previous object
+        EventSystem.current.SetSelectedGameObject(optionsClosedButton);
+    }
+
+    public void OpenControls()
+    {
+        controlsPanel.SetActive(true);
+        mainMenu.SetActive(false);
+        EventSystem.current.SetSelectedGameObject(null); //unselect previous object
+        EventSystem.current.SetSelectedGameObject(controlsFirstButton);
+    }
+
+    public void CloseControls()
+    {
+        controlsPanel.SetActive(false);
+        mainMenu.SetActive(true);
+        EventSystem.current.SetSelectedGameObject(null); //unselect previous object
+        EventSystem.current.SetSelectedGameObject(controlsClosedButton);
+    }
+
     public void QuitGame() {
         Application.Quit();
+        Debug.Log("Quitting");
     }
 }
