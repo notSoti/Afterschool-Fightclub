0712b7e0a508b3e2ce1dad3b7bc3c25c53e861a2
Soti <58138432+notSoti@users.noreply.github.com>
2025-05-23 21:07:47 +0300

    lint

--- My project/Assets/Scripts/ControllerMovements.cs
+++ My project/Assets/Scripts/ControllerMovements.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 
-public class ControllerMovements : MonoBehaviour {
+public class ControllerMovements : MonoBehaviour
+{
     public float moveSpeed = 7f;
     public float jumpForce = 7f;
     public float mainAttackCooldown = 1f;
@@ -14,8 +15,10 @@ public class ControllerMovements : MonoBehaviour {
     private UltimateAbility ultimateAbility;
     private Collider2D hitboxCollider;
 
-    void Start() {
-        if (!gameObject.name.Contains("Player")) {
+    void Start()
+    {
+        if (!gameObject.name.Contains("Player"))
+        {
             enabled = false;
             return;
         }
@@ -26,35 +29,45 @@ public class ControllerMovements : MonoBehaviour {
 
         // Find and set up hitbox for attacks
         Transform hitboxTransform = transform.Find("Player Hitbox");
-        if (hitboxTransform != null) {
+        if (hitboxTransform != null)
+        {
             hitboxCollider = hitboxTransform.GetComponent<Collider2D>();
-            if (hitboxCollider == null) {
+            if (hitboxCollider == null)
+            {
                 Debug.LogError($"No Collider2D found on Player Hitbox for {gameObject.name}");
-            } else {
+            }
+            else
+            {
                 hitboxCollider.enabled = false; // Make sure hitbox starts disabled
             }
             // Set up the hitbox owner
-            if (hitboxTransform.TryGetComponent<Hitbox>(out var hitbox)) {
+            if (hitboxTransform.TryGetComponent<Hitbox>(out var hitbox))
+            {
                 hitbox.owner = gameObject;
             }
-        } else {
+        }
+        else
+        {
             Debug.LogError($"No Player Hitbox found for {gameObject.name}");
         }
 
         // Set up the hurtbox owner
         Transform hurtboxTransform = transform.Find("Player Hurtbox");
-        if (hurtboxTransform != null && hurtboxTransform.TryGetComponent<Hurtbox>(out var hurtbox)) {
+        if (hurtboxTransform != null && hurtboxTransform.TryGetComponent<Hurtbox>(out var hurtbox))
+        {
             hurtbox.owner = gameObject;
         }
     }
 
-    void Update() {
+    void Update()
+    {
         mainAttackTimer -= Time.deltaTime;
         kickTimer -= Time.deltaTime;
 
         bool isFalling = rb.linearVelocity.y <= -0.01f || (!isGrounded && rb.linearVelocity.y < 0.2f);
 
-        if (isFalling) {
+        if (isFalling)
+        {
             CancelMove();
         }
 
@@ -65,23 +78,28 @@ public class ControllerMovements : MonoBehaviour {
         animator.SetBool("isWalking", Mathf.Abs(moveHorizontal) > 0.1f);
 
         // flip the character
-        if (moveHorizontal > 0.1f) {
+        if (moveHorizontal > 0.1f)
+        {
             // facing left
             transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
-        } else if (moveHorizontal < -0.1f) {
+        }
+        else if (moveHorizontal < -0.1f)
+        {
             // facing right
             transform.localScale = new Vector3(-Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
         }
 
         // jump (dpad up)
-        if ((Input.GetAxis("Vertical") > 0.5f || Input.GetKey(KeyCode.Space)) && isGrounded) {
+        if ((Input.GetAxis("Vertical") > 0.5f || Input.GetKey(KeyCode.Space)) && isGrounded)
+        {
             isGrounded = false;
             animator.SetTrigger("Jump");
             rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
         }
 
         // crouch (dpad down)
-        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.JoystickButton2) && isGrounded) {
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.JoystickButton2) && isGrounded)
+        {
             animator.SetTrigger("Crouch Down");
             animator.SetBool("isCrouching", true);
             AnimatorStateInfo state = animator.GetCurrentAnimatorStateInfo(0);
@@ -89,17 +107,21 @@ public class ControllerMovements : MonoBehaviour {
             {
                 animator.speed = 0f;
             }
-        } else {
+        }
+        else
+        {
             animator.ResetTrigger("Crouch Up");
             animator.SetBool("isCrouching", false);
             AnimatorStateInfo state = animator.GetCurrentAnimatorStateInfo(0);
-            if (state.IsName("Crouch Down") && state.normalizedTime > 0.95f && state.normalizedTime < 1f) {
+            if (state.IsName("Crouch Down") && state.normalizedTime > 0.95f && state.normalizedTime < 1f)
+            {
                 animator.speed = 1f;
             }
         }
 
         // main attack (X)
-        if ((Input.GetKeyDown(KeyCode.JoystickButton1) || Input.GetKeyDown(KeyCode.E)) && !isFalling && mainAttackTimer <= 0f) {
+        if ((Input.GetKeyDown(KeyCode.JoystickButton1) || Input.GetKeyDown(KeyCode.E)) && !isFalling && mainAttackTimer <= 0f)
+        {
             mainAttackTimer = mainAttackCooldown;  // Set cooldown first
             animator.SetBool("isAttacking", true);
             animator.SetTrigger("Main Attack");
@@ -107,7 +129,8 @@ public class ControllerMovements : MonoBehaviour {
         }
 
         // kick (square)
-        if ((Input.GetKeyDown(KeyCode.JoystickButton0) || Input.GetKeyDown(KeyCode.Q)) && !isFalling && kickTimer <= 0f) {
+        if ((Input.GetKeyDown(KeyCode.JoystickButton0) || Input.GetKeyDown(KeyCode.Q)) && !isFalling && kickTimer <= 0f)
+        {
             kickTimer = kickCooldown;  // Set cooldown first
             animator.SetBool("isKicking", true);
             animator.SetTrigger("Kick");
@@ -115,36 +138,44 @@ public class ControllerMovements : MonoBehaviour {
         }
 
         // ultimate move (R)
-        if (ultimateAbility != null && !isFalling && ultimateAbility.IsUltimateReady() && (Input.GetKeyDown(KeyCode.R) || Input.GetKeyDown(KeyCode.JoystickButton3))) {
+        if (ultimateAbility != null && !isFalling && ultimateAbility.IsUltimateReady() && (Input.GetKeyDown(KeyCode.R) || Input.GetKeyDown(KeyCode.JoystickButton3)))
+        {
             ultimateAbility.UseUltimate();
         }
     }
 
     // This ensures the animations don't get weird after mid air moves
-    public void OnMoveEnd() {
+    public void OnMoveEnd()
+    {
         CancelMove();
     }
 
-    public void EnableHitbox() {
+    public void EnableHitbox()
+    {
         // Debug.Log($"Enabling hitbox for {gameObject.name}");
-        if (hitboxCollider != null) {
+        if (hitboxCollider != null)
+        {
             hitboxCollider.enabled = true;
 
-            if (hitboxCollider.TryGetComponent<Hitbox>(out var hitbox)) {
+            if (hitboxCollider.TryGetComponent<Hitbox>(out var hitbox))
+            {
                 hitbox.ResetHits();
             }
         }
     }
 
 
-    public void DisableHitbox() {
+    public void DisableHitbox()
+    {
         // Debug.Log($"Disabling hitbox for {gameObject.name}");
-        if (hitboxCollider != null && hitboxCollider != GetComponent<Collider2D>()) {
+        if (hitboxCollider != null && hitboxCollider != GetComponent<Collider2D>())
+        {
             hitboxCollider.enabled = false;
         }
     }
 
-    void CancelMove() {
+    void CancelMove()
+    {
         animator.ResetTrigger("Kick");
         animator.ResetTrigger("Main Attack");
         animator.ResetTrigger("Jump");
@@ -152,8 +183,10 @@ public class ControllerMovements : MonoBehaviour {
         animator.SetBool("isAttacking", false);
     }
 
-    void OnCollisionEnter2D(Collision2D collision) {
-        if (collision.gameObject.CompareTag("Ground")) {
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
             isGrounded = true;
         }
     }
